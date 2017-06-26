26192f5690f3a7dd471e9a5479c6c4402db816af
Darrell Teo <darrelltzj@gmail.com>
2017-06-26 22:51:05 +0800

    removing image types

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,10 +9,10 @@ class Product < ApplicationRecord
   validate :original_greater_than_discount
 
   has_attached_file :image, styles: {
-    thumb: '100x100>',
-    square: '200x200#',
-    medium: '300x300>',
-    large: '600x600>'
+    # thumb: '100x100>',
+    # square: '200x200#',
+    # medium: '300x300>',
+    # large: '600x600>'
   }
 
   # Validate the attached image is image/jpg, image/png, etc
