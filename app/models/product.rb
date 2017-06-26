f1db0b91f60671f09d573a0e9a1b172eaac81491
Darrell Teo <darrelltzj@gmail.com>
2017-06-26 23:05:20 +0800

    troubleshooting image

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,12 +8,13 @@ class Product < ApplicationRecord
 
   validate :original_greater_than_discount
 
-  has_attached_file :image, styles: {
+  # has_attached_file :image, styles: {
     # thumb: '100x100>',
     # square: '200x200#',
     # medium: '300x300>',
     # large: '600x600>'
-  }
+  # }
+  has_attached_file :image
 
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
